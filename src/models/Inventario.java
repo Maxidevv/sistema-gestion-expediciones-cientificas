ffabb5945a9e1a3a97974c9350b9b105442c69f4
Maxi Moncada <maxilucianomoncada@gmail.com>
2026-04-24 09:52:56 -0300

    Cambios

--- src/models/Inventario.java
+++ src/models/Inventario.java
@@ -1,6 +1,13 @@
 package src.models;
 import java.util.ArrayList;
 
+/**
+ * @author Maxi Moncada
+ * @version 1.0
+ * @see InventarioCritico
+ * @see GestionExpedicion
+ */
+
 public class Inventario {
     private ArrayList <herramienta> herramientas;
 

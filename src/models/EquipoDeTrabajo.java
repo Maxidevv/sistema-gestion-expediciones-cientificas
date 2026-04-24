2264b49d8f3df4e1cd2576ffef9dac1a28382a57
matiasmoyanobustos <matiasmoyanobustos@gmail.com>
2026-04-24 18:47:40 -0300

    Add files via upload

---
+++ src/models/EquipoDeTrabajo.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+public class EquipoDeTrabajo {
+    private String nombreEquipo;
+    private ArrayList<INVESTIGADOR> integrantes;
+    private INVESTIGADOR lider;
+
+    public EquipoDeTrabajo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+        this.integrantes = new ArrayList<>();
+        this.lider = null;
+    }
+
+    // Añadir investigador
+    public void agregarInvestigador(INVESTIGADOR inv) {
+        if (lider == null) {
+            lider = inv;//el primero en ser agregado va a ser lider
+            integrantes.add(inv);
+            System.out.println(inv.getNombre() + " es el líder del equipo.");
+        } else {
+            // Si el nuevo investigador tiene mismo rango que el líder, se degrada
+            if (inv.getRango().equalsIgnoreCase(lider.getRango())) {
+                inv.setRango("Sub-" + inv.getRango());
+                System.out.println(inv.getNombre() + " fue degradado a " + inv.getRango() + " porque ya existe un líder con ese rango.");
+            }
+            integrantes.add(inv);
+        }
+    }
+
+    public void quitarInvestigador(INVESTIGADOR inv) {
+        integrantes.remove(inv);
+        if (inv == lider) {
+            lider = null;
+            System.out.println("El líder fue eliminado. El equipo queda sin líder.");
+        }
+    }
+
+    public INVESTIGADOR buscarPorNombre(String nombre) {
+        for (INVESTIGADOR inv : integrantes) {
+            if (inv.getNombre().equalsIgnoreCase(nombre)) {
+                return inv;
+            }
+        }
+        return null;
+    }
+
+    public void mostrarEquipo() {
+        System.out.println("Equipo: " + nombreEquipo);
+        for (INVESTIGADOR inv : integrantes) {
+            System.out.println("- " + inv.getNombre() + " (" + inv.getRango() + ", " + inv.getEspecialidad() + ")");
+        }
+        if (lider != null) {
+            System.out.println("Líder actual: " + lider.getNombre() + " (" + lider.getRango() + ")");
+        } else {
+            System.out.println("El equipo no tiene líder.");
+        }
+    }
+}
